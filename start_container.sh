160f47ff5fb2e4951a06a479ea07329a2532ba0a
Ubuntu <azureuser@RGUbuntu.oymdcvjlirqeni2vcqfxm55tpb.bx.internal.cloudapp.net>
2020-12-11 00:44:44 +0000

    updated haproxy config

---
+++ start_container.sh
@@ -0,0 +1,4 @@
+docker run -p 2222:9999 -d -e APPID=2222 mynodeapp:1.0
+docker run -p 3333:9999 -d -e APPID=3333 mynodeapp:1.0
+docker run -p 4444:9999 -d -e APPID=4444 mynodeapp:1.0
+docker run -p 5555:9999 -d -e APPID=5555 mynodeapp:1.0
